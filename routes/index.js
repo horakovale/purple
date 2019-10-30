09f1803bbc0d2958acc5308ac2df2e4714501bc8
Lenka Horakova <lenda.hor@gmail.com>
2019-10-30 20:53:12 +0100

    implement conversion

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const fetch = require("node-fetch");
 
+const apiKey = '10ea24fb9c7a53b0e0e767eb408d2cc8';
+const apiUrl = 'http://data.fixer.io/api/';
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-  console.log(req.query.amount);
-  console.log(req.query.code);
-
-  const apiKey = '10ea24fb9c7a53b0e0e767eb408d2cc8';
-  const apiUrl = 'http://data.fixer.io/api/';
 
   let query = fetch(`${apiUrl}latest?access_key=${apiKey}`);
 
   query
     .then(response => response.json())
-    .then(displayCurrencyCodes)
+    .then(data => displayData(data, req.query.amount, req.query.sourceCode, req.query.destinationCode))
     .catch(error => {
-      console.log('Došlo k chybě', error);
-      next(error)
+      console.log(error);
+      next(error);
+    });
+
+  function displayData(data, amount, sourceCode, destinationCode) {
+
+    let result = "";
+    if (sourceCode && amount && destinationCode ) {
+      result = ((amount / data.rates[sourceCode]) * data.rates[destinationCode]).toString();
     }
-    );
 
-  function displayCurrencyCodes(data) {
-    let html = '';
-
-    for (var key in data.rates) {
-      html = html + `
-                <option>
-                ${key}
-                </option>      
-                `;
-    };
-
-    res.render('index', { title: 'Currency conversion app', currencyList: html });
+    res.render('index', {
+      title: 'Currency conversion app',
+      sourceCurrencyList: currencyList(sourceCode, data),
+      destinationCurrencyList: currencyList(destinationCode, data),
+      amount: amount,
+      result: result
+    });
   }
 
 });
+
+function currencyList(code, data) {
+  let html= "";
+  for (var key in data.rates) {
+
+    if (key === code) {
+      html = html + `
+       <option selected>
+       ${key}
+       </option>      
+     `;
+    } else {
+      html = html + `
+       <option>
+       ${key}
+       </option>      
+     `;
+    }
+  };
+  return html;
+}
 
 module.exports = router;
