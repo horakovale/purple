5db362286a10f074dd0dadfcb8b1548f1246a7bd
Lenka Horakova <lenda.hor@gmail.com>
2019-10-30 18:43:39 +0100

    fetch currency codes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+const fetch = require("node-fetch");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  const apiKey = '10ea24fb9c7a53b0e0e767eb408d2cc8';
+  const apiUrl = 'http://data.fixer.io/api/';
+
+  let query = fetch(`${apiUrl}latest?access_key=${apiKey}`);
+
+  query
+    .then(response => response.json())
+    .then(displayCurrencyCodes)
+    .catch(error => {
+      console.log('Došlo k chybě', error);
+      next(error)
+    }
+    );
+
+  function displayCurrencyCodes(data) {
+    let html = '';
+
+    console.log(data);
+
+    for (var key in data.rates) {
+      html = html + `
+                <option>
+                ${key}
+                </option>      
+                `;
+    };
+
+    res.render('index', { title: 'Currency conversion app', currencyList: html });
+  }
+
 });
 
 module.exports = router;
